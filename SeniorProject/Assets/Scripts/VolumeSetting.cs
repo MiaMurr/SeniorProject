b38e6583d68f8060be5a360bd9693681daae00f4
Alrick96 <112648005+Alrick96@users.noreply.github.com>
2023-02-14 18:52:13 -0500

    update

--- SeniorProject/Assets/Scripts/VolumeSetting.cs
+++ SeniorProject/Assets/Scripts/VolumeSetting.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class VolumeSetting : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] protected float MusicVolume = 1f;
+    [SerializeField] protected AudioMixer audioMixer;
     void Start()
     {
-        
+        MusicVolume = PlayerPrefs.GetFloat("volume");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        PlayerPrefs.SetFloat("volume", MusicVolume);
     }
 }
