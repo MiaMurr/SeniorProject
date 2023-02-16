949da8ead4f96363faf09340d08ffa0b68be4de2
EvWills <evwills6@gmail.com>
2023-02-15 23:36:55 -0500

    SnakeGamew/Movement-EW

--- SeniorProject/Assets/Snake.cs
+++ SeniorProject/Assets/Snake.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Snake : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private Vector3 _direction = Vector3.right;
+
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            _direction = Vector3.up;
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            _direction = Vector3.down;
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            _direction = Vector3.left;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            _direction = Vector3.right;
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        this.transform.position = new Vector3(
+        Mathf.Round(this.transform.position.x) + _direction.x,
+        Mathf.Round(this.transform.position.y) + _direction.y,
+        Mathf.Round(this.transform.position.z) + _direction.z);
     }
 }
+    
