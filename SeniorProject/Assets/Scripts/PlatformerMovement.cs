d749ddc760ae1c2c88e239bb9aab4fecfd004ed6
Alrick96 <112648005+Alrick96@users.noreply.github.com>
2023-03-17 23:58:02 -0400

    player-controls-update

--- SeniorProject/Assets/Scripts/PlatformerMovement.cs
+++ SeniorProject/Assets/Scripts/PlatformerMovement.cs
@@ -5,21 +5,35 @@ using UnityEngine.InputSystem;
 
 public class PlatformerMovement : MonoBehaviour
 {
-    public GameObject Player;
-    public float speed;
-    private Vector3 _direction;
+    public Rigidbody player;
+    //public GameObject Player;
+    [SerializeField]private float speed;
+    private Vector3 playerMove;
+    [SerializeField] private float snakeTurn;
+
     public void OnMovement(InputValue input) // in the input menu the function is created with all properties 
                                              //relating to buttion behavoiour
     {
-        _direction = input.Get<Vector3>(); // WASD is connected to a certian direction of vector 3
+        playerMove = input.Get<Vector3>(); // WASD is connected to a certian direction of vector 3
     }
+
+    void Update()
+    {
+        moveplayer();
+    }
+
     void moveplayer()
-    {   // If makes it that once rotation is done it will stay in that direction
-        if (_direction.magnitude == 0)
+    {
+
+        // If makes it that once rotation is done it will stay in that direction
+        if (playerMove.magnitude == 0)
         {
             return;
         }
-        var rotation = Quaternion.LookRotation(_direction);
-        //transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, snakeTurn);
+        var rotation = Quaternion.LookRotation(playerMove);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, snakeTurn);
+
+        transform.Translate(playerMove * speed * Time.deltaTime, Space.World);
+        //player.MovePosition(transform.position + playerMove * Time.deltaTime * speed);
     }
 }
