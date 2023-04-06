c3e3b8483ab6c5c5ac52ce409beaa376907c8fd8
MMCollegeCode <104695429+MMCollegeCode@users.noreply.github.com>
2023-04-05 23:14:54 -0400

    Revert "Mia - Timer, Camera Switch, End Goal"
    This reverts commit 86f9a501e456fa9d8fb6a52d42bf06822e1623c4.

--- SeniorProject/Assets/Scripts/Platformer/PlatformerTimer.cs
+++ SeniorProject/Assets/Scripts/Platformer/PlatformerTimer.cs
@@ -8,9 +8,7 @@ public class PlatformerTimer : MonoBehaviour
 {
 
     public float time;
-    public float time2;
     public TextMeshProUGUI timeText;
-    public TextMeshProUGUI timeText2;
 
 
 
@@ -18,19 +16,15 @@ public class PlatformerTimer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        time = 0;
-        time2 = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        time += 1 * Time.deltaTime;
-        timeText.text = time.ToString("00");
-        if (time >= 60)
+        time -= 1 * Time.deltaTime;
+        timeText.text = time.ToString("0");
+        if (time <= 0)
         {
-            time2++;
-            timeText2.text = time2.ToString("00");
             time = 0;
         }
     }
