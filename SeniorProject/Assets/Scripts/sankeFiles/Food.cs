f99580714416cba4370835df76282b5f9ab6100b
Alrick96 <112648005+Alrick96@users.noreply.github.com>
2023-04-27 15:45:04 -0400

    foodPickupFix

--- SeniorProject/Assets/Scripts/sankeFiles/Food.cs
+++ SeniorProject/Assets/Scripts/sankeFiles/Food.cs
@@ -8,14 +8,36 @@ public class Food : MonoBehaviour
     public BoxCollider GridView;
     //Here you can set the value of the snake food Item
     public float ScoreValue = 10f;
-    
+
+    private Vector3 Origin = new Vector3();
+
+    [SerializeField] private float RotationbySecond = 10f;
+    [SerializeField] private float Frequency = 1f;
+    [SerializeField] private float Hight = 3f;
+    private bool state;
+
 
     private void Start() {
+        
         SnakeScore.score = 0f;
         SnakeScore.Lives = 3f;
         RandomLocation();
+        state = true;
+        
+        
+
+
     }
 
+    private void Update()
+    {
+        if (state) {
+        Vector3 tmpLocation = Origin;
+        tmpLocation.y += Mathf.Sin(Time.fixedTime * Mathf.PI * Frequency) * Hight;
+        transform.Rotate(new Vector3(0f, Time.deltaTime * RotationbySecond, 0f), Space.World);
+        transform.position = tmpLocation;
+        }
+    }
     private void RandomLocation() {
 
         Bounds bounds = this.GridView.bounds;
@@ -25,13 +47,17 @@ public class Food : MonoBehaviour
         float z = Random.Range(bounds.min.z, bounds.max.z);
 
         this.transform.position = new Vector3(Mathf.Round(x), Mathf.Round(y), Mathf.Round(z));
+        Origin = transform.position;
     }
 
     private void OnTriggerEnter(Collider other) {
 
         if (other.tag == "Player") {
             SnakeScore.score += ScoreValue;
+            state = false;
             RandomLocation();
+            state = true;
+            
 
         }
         
